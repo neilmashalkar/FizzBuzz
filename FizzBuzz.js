321d817733aab61fcf3fa67683cf61c374e0f19c
Neil Mashalkar <neilmashalkar@gmail.com>
2025-09-02 13:57:46 -0500

    Added comments

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,22 @@
-for (let i = 1; i <= 100; i++){
-    if (i%5==0 && i%3==0){
-        console.log("FizzBuzz")
-    }
-    else if (i%3 == 0) {
-        console.log("Fizz")
-    }
-    else if (i%5==0){
-        console.log("Buzz")
-    }
-    
-    else{
-        console.log(i)
+//Created by Neil Mashalkar on 9/2/2025
+
+// Loop through the numbers 1 to 100
+for (let i = 1; i <= 100; i++) {
+
+    // If the number is divisible by both 3 AND 5
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log("FizzBuzz"); // Print "FizzBuzz"
+
+    // If the number is divisible by 3 only
+    } else if (i % 3 == 0) {
+        console.log("Fizz"); // Print "Fizz"
+
+    // If the number is divisible by 5 only
+    } else if (i % 5 == 0) {
+        console.log("Buzz"); // Print "Buzz"
+
+    // Otherwise (not divisible by 3 or 5)
+    } else {
+        console.log(i); // Print the number itself
     }
 }
